06a1f8bcd1a882e2e75341dd70f24826febf0115
João Pedro Pereira <joao.pedro.pereira@tecnico.ulisboa.pt>
2017-03-21 12:26:17 +0000

    First try without the big tutorial offered by Miguel

---
+++ Formacao Pong/Pong/Assets/Scripts/BallController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private Transform tr;
+	private GameObject player;
+	private Transform playerTr;
+	public float speed;
+
+	private const float MAX_BOUNCE_ANGLE = 5 * Mathf.PI / 12; //Equivalent to 75 degrees
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		tr = GetComponent<Transform> ();
+
+		//TODO change this code because it is ugly
+		player = GameObject.FindGameObjectsWithTag("Player")[0];
+
+		playerTr = player.GetComponent<Transform> ();
+
+		rb.velocity = (new Vector3(-1 * 15, 0.0f, 0.0f));
+	}
+
+	void OnCollisionEnter (Collision col) {
+		Vector3 vel;
+		if (col.gameObject.tag == "Player") {
+			vel = CalculateVelocityAgainstPlayer (playerTr);
+			rb.velocity = speed * (vel.normalized);
+		}
+
+		if (col.gameObject.tag == "Wall") {
+			vel = new Vector3(
+				rb.velocity.x,
+				-rb.velocity.y,
+				0.0f
+			);
+			rb.velocity = speed * (vel.normalized);
+		}
+	}
+
+	Vector3 CalculateVelocityAgainstPlayer(Transform trans) {
+		float relativeIntersectY = (trans.position.y + (trans.localScale.y / 2)) - tr.position.y;
+		float normalizedRelativeIntersectionY = relativeIntersectY / (trans.localScale.y / 2);
+		float bounceAngle = normalizedRelativeIntersectionY * MAX_BOUNCE_ANGLE;
+
+		Vector3 vel = new Vector3 (
+			speed * Mathf.Sin(bounceAngle), 
+			speed * Mathf.Cos(bounceAngle),
+			0.0f
+		);
+		return vel;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Formacao Pong/Pong/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float yMin, yMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private Transform tr;
+	public float speed;
+	public Boundary boundary;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		tr = GetComponent<Transform> ();
+	}
+
+	void FixedUpdate () {
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (0.0f, moveVertical, 0.0f);
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3 (
+			tr.position.x,
+			Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax),
+			0.0f
+		);
+	}
+}
